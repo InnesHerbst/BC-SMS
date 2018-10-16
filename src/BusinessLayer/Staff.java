f911498b922e4fb51b8c7319d3f5d0d6dc81939a
Innes Herbst <innes.herbst@gmail.com>
2018-10-16 12:11:35 +0200

    Fixed constructor

--- src/BusinessLayer/Staff.java
+++ src/BusinessLayer/Staff.java
@@ -8,12 +8,13 @@ import java.util.Date;
  */
 public class Staff extends Person{
 
-    public Staff(int p_ID, String p_initials, String p_firstname, String p_lastname, Date p_dob, int p_gender, int p_phone, String p_email, String p_password, String p_address1, String p_address2,int campus_id,int department_id) {
+    public Staff(int campus_id, int department_id, String p_ID, String p_initials, String p_firstname, String p_lastname, Date p_dob, String p_gender, String p_phone, String p_email, String p_password, String p_address1, String p_address2) {
         super(p_ID, p_initials, p_firstname, p_lastname, p_dob, p_gender, p_phone, p_email, p_password, p_address1, p_address2);
-        
         this.campus_id = campus_id;
-        this.department_id = this.department_id;
+        this.department_id = department_id;
     }
+
+    
     
     private int campus_id;
     private int department_id;
