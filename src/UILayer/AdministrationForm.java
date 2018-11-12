fb72e74417ecbc655d32056d94e3ce1f4d23c6a5
innes <innes@192.168.1.8>
2018-11-12 16:01:43 +0200

    Added Un Auth Stock Table

--- src/UILayer/AdministrationForm.java
+++ src/UILayer/AdministrationForm.java
@@ -189,6 +189,9 @@ public class AdministrationForm extends javax.swing.JFrame {
         btnSearch = new javax.swing.JToggleButton();
         jButton6 = new javax.swing.JButton();
         jButton7 = new javax.swing.JButton();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        tblUnAuthStock = new javax.swing.JTable();
+        btnAuthOrder = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         jButton2 = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
@@ -309,6 +312,40 @@ public class AdministrationForm extends javax.swing.JFrame {
             }
         });
 
+        tblUnAuthStock.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "ID", "Name", "Price", "Quantity", "Category", "Description"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane4.setViewportView(tblUnAuthStock);
+        if (tblUnAuthStock.getColumnModel().getColumnCount() > 0) {
+            tblUnAuthStock.getColumnModel().getColumn(0).setResizable(false);
+            tblUnAuthStock.getColumnModel().getColumn(0).setPreferredWidth(0);
+            tblUnAuthStock.getColumnModel().getColumn(1).setResizable(false);
+            tblUnAuthStock.getColumnModel().getColumn(2).setResizable(false);
+            tblUnAuthStock.getColumnModel().getColumn(3).setResizable(false);
+            tblUnAuthStock.getColumnModel().getColumn(4).setResizable(false);
+            tblUnAuthStock.getColumnModel().getColumn(5).setResizable(false);
+        }
+
+        btnAuthOrder.setText("Authorize Stock");
+        btnAuthOrder.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAuthOrderActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -340,7 +377,9 @@ public class AdministrationForm extends javax.swing.JFrame {
                                 .addGap(18, 18, 18)
                                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(36, 36, 36)
-                                .addComponent(jButton7)))
+                                .addComponent(jButton7))
+                            .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 706, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnAuthOrder))
                         .addGap(0, 114, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -362,14 +401,18 @@ public class AdministrationForm extends javax.swing.JFrame {
                     .addComponent(txtSearch, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnSearch))
                 .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 393, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton4)
                     .addComponent(jButton3)
                     .addComponent(jButton1)
                     .addComponent(jButton7))
-                .addContainerGap())
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btnAuthOrder)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         tbpAdmin.addTab("Stock", jPanel1);
@@ -499,7 +542,7 @@ public class AdministrationForm extends javax.swing.JFrame {
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(15, Short.MAX_VALUE)
+                .addContainerGap(83, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
                     .addComponent(cmbSortEmploy, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -535,7 +578,7 @@ public class AdministrationForm extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(tbpAdmin)
+                .addComponent(tbpAdmin, javax.swing.GroupLayout.DEFAULT_SIZE, 627, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -757,6 +800,10 @@ public class AdministrationForm extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_txtSearchActionPerformed
 
+    private void btnAuthOrderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAuthOrderActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnAuthOrderActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -823,6 +870,7 @@ public class AdministrationForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAuthOrder;
     private javax.swing.JButton btnSEmploy;
     private javax.swing.JToggleButton btnSearch;
     private javax.swing.JComboBox<String> cmbSort;
@@ -843,9 +891,11 @@ public class AdministrationForm extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
     private javax.swing.JTable jtblStaff;
     private javax.swing.JTable jtblStaffUn;
     private javax.swing.JTable jtblStock;
+    private javax.swing.JTable tblUnAuthStock;
     private javax.swing.JTabbedPane tbpAdmin;
     private javax.swing.JTextField txtSEmploy;
     private javax.swing.JTextField txtSearch;
