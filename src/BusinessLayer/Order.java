71c4720ff6c9fed65e8783803be4cb02bde3daf4
innes <innes@192.168.1.8>
2018-11-12 15:54:36 +0200

    Added Stuff for Orders

--- src/BusinessLayer/Order.java
+++ src/BusinessLayer/Order.java
@@ -16,7 +16,6 @@ public class Order implements Serializable{
     private Date orderDate;
     private int orderQty;
     private Date pickUpDate;
-    private OrderDataHandler dbConn;
 
     public Order(int orderID, String orderDesc, Date orderDate, int orderQty, Date pickUpDate) {
         this.orderID = orderID;
@@ -24,7 +23,6 @@ public class Order implements Serializable{
         this.orderDate = orderDate;
         this.orderQty = orderQty;
         this.pickUpDate = pickUpDate;
-        this.dbConn = new OrderDataHandler();
     }
 
     public int getOrderID() {
@@ -65,28 +63,6 @@ public class Order implements Serializable{
 
     public void setPickUpDate(Date pickUpDate) {
         this.pickUpDate = pickUpDate;
-    }
-
-    public Order getOrder(int id) throws ClassNotFoundException, SQLException {
-        Order stock = dbConn.GetOrder(getOrderID());
-        return stock;
-    }
-
-    public boolean addOrder() throws SQLException, ClassNotFoundException {
-        return dbConn.InsertOrder(this);
-    }
-
-    public boolean updateOrder() throws SQLException, ClassNotFoundException {
-        return dbConn.UpdateOrder(this);
-    }
-
-    public boolean removeOrder() throws SQLException, ClassNotFoundException {
-        return dbConn.DeleteOrder(getOrderID());
-    }
-
-    @Override
-    public String toString() {
-        return "Order{" + "orderID=" + orderID + ", orderDesc=" + orderDesc + ", orderDate=" + orderDate + ", orderQty=" + orderQty + ", pickUpDate=" + pickUpDate + ", dbConn=" + dbConn + '}';
     }
 
 }
--- src/DataLayer/OrderDataHandler.java
+++ src/DataLayer/OrderDataHandler.java
@@ -2,6 +2,9 @@ package DataLayer;
 
 import java.util.ArrayList;
 import BusinessLayer.Order;
+import BusinessLayer.Stock;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -9,28 +12,120 @@ import java.util.List;
  *
  * @author Marline
  */
-public class OrderDataHandler {
+public class OrderDataHandler extends ConnectionHandler {
 
-    public Order GetOrder(int id) throws ClassNotFoundException, SQLException {
-        return null;
+    private static OrderDataHandler orderDataHandler;
+
+    private OrderDataHandler() {
+        super();
     }
 
-    public ArrayList<Order> GetOrder(String... params) throws SQLException, Exception {
-        ArrayList<Order> data = new ArrayList<>();
+    public static OrderDataHandler getInstance() {
+        if (orderDataHandler == null) {
+            orderDataHandler = new OrderDataHandler();
+        }
 
-        return data;
+        return orderDataHandler;
     }
 
-    public boolean InsertOrder(Order order) {
-        return false;
+    public List<Stock> GetAuthOrders() throws ClassNotFoundException, SQLException {
+        List<Stock> stock = new ArrayList<>();
+        PreparedStatement sCMD = null;
+
+        try {
+            if (ConnectDatabase()) {
+                sCMD = getDbConnection().prepareStatement("Execute SP_FetchStock");
+
+                ResultSet result = sCMD.executeQuery();
+
+                while (result.next()) {
+                    int sID = result.getInt("Stock_ID");
+                    String sName = result.getString("Product_Name");
+                    double sPrice = result.getDouble("Product_Price");
+                    int sQuant = result.getInt("product_Quantity");
+                    String sCat = result.getString("Category_Name");
+                    String sCDesc = result.getString("Category_Desc");
+
+                    stock.add(new Stock(sID, sName, sPrice, sQuant, sCat, sCDesc));
+                }
+
+            }
+        } catch (SQLException e) {
+            System.out.println("Problem Occured : " + e.getMessage());
+        } finally {
+            if (sCMD != null) {
+                sCMD.close();
+            }
+
+            DisconnectDatabase();
+        }
+        //stock.add(new Stock(2, "Piet", 18, 5, "new", "new"));
+        return stock;
+    }
+    
+    public List<Stock> GetUnAuthOrders() throws ClassNotFoundException, SQLException {
+        List<Stock> stock = new ArrayList<>();
+        PreparedStatement sCMD = null;
+
+        try {
+            if (ConnectDatabase()) {
+                sCMD = getDbConnection().prepareStatement("Execute SP_FetchUnAutStock");
+
+                ResultSet result = sCMD.executeQuery();
+
+                while (result.next()) {
+                    int sID = result.getInt("Stock_ID");
+                    String sName = result.getString("Product_Name");
+                    double sPrice = result.getDouble("Product_Price");
+                    int sQuant = result.getInt("product_Quantity");
+                    String sCat = result.getString("Category_Name");
+                    String sCDesc = result.getString("Category_Desc");
+
+                    stock.add(new Stock(sID, sName, sPrice, sQuant, sCat, sCDesc));
+                }
+
+            }
+        } catch (SQLException e) {
+            System.out.println("Problem Occured : " + e.getMessage());
+        } finally {
+            if (sCMD != null) {
+                sCMD.close();
+            }
+
+            DisconnectDatabase();
+        }
+        //stock.add(new Stock(2, "Piet", 18, 5, "new", "new"));
+        return stock;
     }
 
-    public boolean UpdateOrder(Order order) throws SQLException, ClassNotFoundException {
-        return false;
-    }
-
-    public boolean DeleteOrder(int id) throws SQLException, ClassNotFoundException {
-        return false;
-
+    public boolean UpdateOrder(int stockID) throws SQLException, ClassNotFoundException {
+        PreparedStatement uCMD = null;
+        
+        try {
+            ConnectDatabase();
+            
+            uCMD = getDbConnection().prepareStatement("Update Stock set stock_confirmation = 'Yes' where Stock_ID= '?'");
+            uCMD.setInt(0, stockID);
+            
+            int count = uCMD.executeUpdate();
+            
+            if(count != 1){
+                return false;
+            }
+            
+        } catch (Exception e) {
+        }finally{
+            try{
+                if(uCMD != null){
+                    uCMD.close();
+                }
+            }catch (SQLException sqle){
+                
+            }
+            
+            DisconnectDatabase();
+        }
+        
+        return true;
     }
 }
